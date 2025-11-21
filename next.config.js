551d88c078734aba8928064a01bfc4adca20e94e
Mohammed Irfan <mailtomohammedirfank@gmail.com>
2025-11-21 17:28:30 +0530

    Update image domains to use production domain instead of localhost

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'images.unsplash.com'],
+    domains: ['campuseatsmec.netlify.app', 'images.unsplash.com'],
   },
 }
 
